eadd69a85d8f5bec84527a05a88faa0d95e6bcf2
nxrxka <154493227+nxrxka@users.noreply.github.com>
2023-12-26 10:43:09 +0900

    Update weblogparser_IIS.py

--- weblogparser_IIS.py
+++ weblogparser_IIS.py
@@ -2,10 +2,10 @@ import sqlite3
 import time
 
 def create_database():
-    output_name = f'-weblogresult-/result_weblog_{time.time()}.db'
+    output_name = f'weblogresult/result_weblog_{time.time()}.db'
     conn = sqlite3.connect(output_name)
     cursor = conn.cursor()
-    sql = f"CREATE TABLE Result(Local_IP text, Client_IP text, Date text, Method text, URL text, Argument text, Port text, Status_Code text, Size text, User_Agent text, Refer text, File_Name text)"
+    sql = f"CREATE TABLE IIS_log(Local_IP text, Client_IP text, Date text, Method text, URL text, Argument text, Port text, Status_Code text, Size text, User_Agent text, Refer text, File_Name text)"
     cursor.execute(sql)
     return cursor, conn
 
@@ -30,7 +30,7 @@ def log_parse(contents, input_val, cursor):
         with open(f"{file_name}_parsing_error", 'a') as f2:
             f2.write(f"split error({e}): {contents}")
         return
-    sql = f"INSERT INTO Result(Local_IP, Client_IP, Date, Method, URL, Argument, Port, Status_Code, Size, User_Agent, Refer, File_Name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    sql = f"INSERT INTO IIS_log(Local_IP, Client_IP, Date, Method, URL, Argument, Port, Status_Code, Size, User_Agent, Refer, File_Name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
     val = (
         val_local_ip, val_client_ip, val_date, val_method, val_url, val_url_argu, val_port, val_status, val_size,
         val_useragent, val_refer, file_name)
@@ -48,7 +48,7 @@ def read_file_contents(input_val, cursor):
         print("An error occurred:", e)
 
 def main():
-    input_val = "/Users/-userID-/-weblogtypes-/type1.log"
+    input_val = "/Users/*user*/pycodes/weblogtypes/type1.log"
     cursor, conn = create_database()
     read_file_contents(input_val, cursor)
     conn.commit()
